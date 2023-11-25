73617f3b25016234e43c0905744b2786b389b326
suhani221 <98315116+suhani221@users.noreply.github.com>
2023-11-25 15:35:39 +0530

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,11 @@
+function hoverEffect(element) {
+    element.style.transform = "translateY(-5px)";
+    element.style.boxShadow = "0 5px 15px rgba(0,0,0,0.2)";
+  }
+  
+  function hoverEffectOff(element) {
+    element.style.transform = "translateY(0px)";
+    element.style.boxShadow = "none";
+  }
+  
+  
